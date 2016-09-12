1792e84a29b21936ab7297c569d0872b5ae4f081
Ben Holland <benjholla@gmail.com>
2016-09-12 08:43:07 +0530

    fixed import

--- labs/lab1-ddmin/DeltaDebug/src/dd/examples/XMLParser.java
+++ labs/lab1-ddmin/DeltaDebug/src/dd/examples/XMLParser.java
@@ -1,11 +1,8 @@
 package dd.examples;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
